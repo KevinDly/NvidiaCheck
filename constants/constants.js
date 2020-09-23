a0b800f2a5b2b2e65fa10c34a5a2b590e2f17b23
Saibogu <kmdolay@gmail.com>
2020-09-23 16:44:02 -0700

    Modified gpuSearch() to search through different NVIDIA page

---
+++ constants/constants.js
@@ -0,0 +1,7 @@
+//NVIDIA Site Constants
+export const NVIDIA_URL = 'https://www.nvidia.com/en-us/shop/geforce/?page=1&limit=9&locale=en-us';
+export const PRODUCT_DETAILS_STRING = '.product-details-list-tile';
+export const NVIDIA_OOS = 'featured-buy-link link-btn brand-green  cta-button stock-grey-out';
+export const NVIDIA_INSTOCK = 'featured-buy-link link-btn brand-green cta-button js-add-button';
+export const NVIDIA_GEFORCE_3080 = 'NVIDIA GEFORCE RTX 3080';
+export const OOS = "OUT OF STOCK"
--- index.js
+++ index.js
@@ -1,86 +1,76 @@
 const puppeteer = require('puppeteer');
-const nvidiaURL = 'https://www.nvidia.com/en-us/shop/geforce/?page=1&limit=9&locale=en-us';
-const PRODUCT_DETAILS_STRING = '.product-details-list-tile';
-const NVIDIA_OOS = 'featured-buy-link link-btn brand-green  cta-button stock-grey-out';
+const NVIDIA_STORE = 'https://www.nvidia.com/en-us/shop/geforce/?page=1&limit=9&locale=en-us';
+const CLASS_PRODUCT_DETAILS_STRING = '.product-details-list-tile';
+const CLASS_NVIDIA_OOS = 'featured-buy-link link-btn brand-green  cta-button stock-grey-out';
 const NVIDIA_INSTOCK = 'featured-buy-link link-btn brand-green cta-button js-add-button';
-const TOOLTIP = 'tooltip';
 const NVIDIA_GEFORCE_3080 = 'NVIDIA GEFORCE RTX 3080';
-const OOS = "OUT OF STOCK"
+const CLASS_NOICON = '.noicon';
+const OOS = "OUT OF STOCK";
 const cooldown = 3;
+const NVIDIA_3080_URL = 'https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/'
 
 //TODO: Repeatidly call this script, with timer delay
 //TODO: Need to close page rather than browser after every call
 //TODO: Need to remove browser from gpuSearch and have it created outside.
 //TODO: Need to make function return a promise
+//TODO: Add sound that signals if card was found.
 
 async function gpuSearch(){
     var browser, inStock; 
     try{
         browser = await puppeteer.launch();
         var page = await browser.newPage();
-        await page.goto(nvidiaURL);
         
-        console.log("Went to page!")
+        //Taken from the following:
+        //https://www.scrapehero.com/how-to-increase-web-scraping-speed-using-puppeteer/#:~:text=To%20block%20images%2C%20we%20have%20to%20set%20up%20a%20Request%20Interception.&text=In%20the%20existing%20script%20add,%7D)%3B%20let%20page%20%3D%20await%20browser.
+        await page.setRequestInterception(true);
+        
+        page.on('request', (req) => {
+            if(req.resourceType() === 'image'){
+                req.abort();
+            }
+            else{
+                req.continue();
+            }
+        });
+        
+        await page.goto(NVIDIA_3080_URL,{
+            timeout: 0
+            });
+        
+        console.log("Went to page!");
         
         //Grab the list of document elements that are graphics cards.
-        var doclist = await page.$$(PRODUCT_DETAILS_STRING)
         
-        //Iterate through all graphics card elements until we find the 3080
-        var gpuElement = null;
-        var gpuText = "";
+        var data = await page.evaluate(() => document.body.innerHTML);
+        var doc = await page.$$(CLASS_NOICON);
         
-        for(var i = 0; i < doclist.length; i++) {
-            //Since we are operating on the element we have to await at both points here. Aka, both getProperty and innerText returns promises.
-            var innerText = await doclist[i].getProperty('innerText');
-            var jsonText = await innerText.jsonValue()
-            
-            if(jsonText.includes(NVIDIA_GEFORCE_3080)){
-                console.log(i + " is the 3080 Element");
-                gpuElement = doclist[i]
-                gpuText = jsonText;
-            }
-            else
-                console.log(i + " is not the 3080 Element");
-        }
+        //Might need to iterate if they decide to change the html again.
+        var innerText = await doc[1].getProperty('innerText');
+        var jsonText = await innerText.jsonValue();
+        var jsonUpper = jsonText.toUpperCase();
         
-        //Check if we actually found the 3080
-        if(gpuElement == null){
-            //If we cantg find the 3080, return from function as something must be wrong.
-            console.log("Couldnt find 3080")
-            return;
+        if(jsonUpper.includes(OOS)){
+            console.log("3080 is not in stock!")
         }
         else{
-            /*var instockcheck = await gpuElement.$(NVIDIA_OOS)
-            console.log(instockcheck)*/
-            if(gpuText.includes(OOS)) {
-                console.log("3080 is not in stock!")
-                inStock = false;
-            }
-            else {
-                console.log("3080 is in stock!")
-                inStock = true;
-            }
+            console.log("3080 is in stock!")
         }
-        
-        //Todo: Add sound that plays if stock was found.
-        
-        await browser.close();
-        
         
         //Probably dont close browser every time?
-        console.log("Closed browser!")
-        }
-    catch(err){
-        console.log('Error occurred, could not successfully setup async function');
-        console.log(err)
-        
-        //Check if browser was opened.
     }
-    if(browser != null)
+    catch(err){
+        console.log('Error has occurred somewhere in the gpuSearch function.');
+        console.log(err)
+    }
+    
+    //Close Browser Regardless of Success or Failure
+    if(browser != null) {
         await browser.close();
+        console.log("Closed browser!")
+    }
     
     //return inStock;
 }
 
 gpuSearch();
-
